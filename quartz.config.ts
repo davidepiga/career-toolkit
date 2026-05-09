d9ab1b5208fe37178dc6c145fee21b4d8101c169
pdavide <davidepiga@users.noreply.github.com>
2026-05-09 19:26:18 +0200

    Change pageTitle from 'Career Navigation Toolkit' to 'Career Navigation'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Career Navigation Toolkit",
+    pageTitle: "Career Navigation",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
