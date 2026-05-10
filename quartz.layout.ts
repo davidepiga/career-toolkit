2d5864e8e97cee9f1c3a5628234821eea47cf269
pdavide <davidepiga@users.noreply.github.com>
2026-05-10 17:58:10 +0200

    Remove links from footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,13 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      "About": "/about",
-      "Event coverage analysis": "/event-coverage-analysis",
-      "LinkedIn": "https://www.linkedin.com/in/davidepiga",
-    },
-  }),
+  footer: Component.Footer({ links: {} }),
 }
 
 // components for pages that display a single page (e.g. a single note)
